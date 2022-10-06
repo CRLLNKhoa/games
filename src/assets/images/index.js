95a71870deb28bcadc64bf2cdd30b512f25bfb2f
CRLLNKhoa <lnkhoa12052000@gmail.com>
2022-10-06 12:59:27 +0700

    Updates

--- src/assets/images/index.js
+++
@@ -1,5 +0,0 @@
-const images = {
-    noImage: require('../../assets/images'),
-}
-
-export default images
--- src/layout/components/Search/SearchItem.js
+++
@@ -1,5 +0,0 @@
-function SearchItem({ title, href = '#' }) {
-    return <a href="href">{title}</a>;
-}
-
-export default SearchItem;
